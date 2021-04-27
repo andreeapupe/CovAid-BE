7432d44bdd9a7f384025d34d9a7d5118067ff670
Andreea <andreeapupe@yahoo.com>
2021-04-27 23:28:24 +0300

    First commit

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Andreea Pupe',
+            'email' => 'andreeapupe@yahoo.com',
+            'password' => bcrypt('secret')
+        ]);
+    }
+}
