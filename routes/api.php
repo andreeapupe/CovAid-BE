4860624740cea8e60b3d8463855e101a23d1670f
Andreea <andreeapupe@yahoo.com>
2021-06-10 15:22:51 +0300

    Endpoint that returns user's own appointments

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\DoctorsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->get('/doctors', [DoctorsController::class, 'index']);
+
+Route::middleware('auth:sanctum')->get('/appointments', [UserController::class, 'index']);
